ccc3a2d8c78bdaff85954d7c78d0f7cb83d0c4bf
vonbrank <vonbrank@outlook.com>
2024-03-16 21:45:08 +0800

    feat: 实现 Fiber 机制

--- src/tiny-react/tiny-react.ts
+++ src/tiny-react/tiny-react.ts
@@ -10,7 +10,7 @@ namespace TinyReact {
   }
 
   interface Props {
-    children?: JSX.Element | JSX.Element[];
+    children: TinyReactElement[];
     [index: string]: any;
   }
   type FunctionComponentElementType<P extends Props> = (
@@ -30,6 +30,28 @@ namespace TinyReact {
       children: [];
     };
   }
+
+  interface FiberBase {
+    props: Props;
+    dom: DomType | null;
+    parent?: Fiber;
+    child?: Fiber;
+    sibling?: Fiber;
+  }
+
+  interface NoTypeFiber extends FiberBase {
+    type: undefined;
+  }
+
+  interface CommonFiber extends FiberBase {
+    type: string;
+  }
+
+  interface FunctionComponentFiber extends FiberBase {
+    type: FunctionComponentElementType<any>;
+  }
+
+  type Fiber = NoTypeFiber | CommonFiber | FunctionComponentFiber;
 
   export function createElement(
     type: ElementType,
@@ -61,6 +83,19 @@ namespace TinyReact {
 
   type DomType = (HTMLElement | Text) & { [index: string]: any };
 
+  function createDom(fiber: CommonFiber) {
+    const dom: DomType =
+      fiber.type === "_TEXT_ELEMENT_"
+        ? document.createTextNode("")
+        : document.createElement(fiber.type);
+
+    Object.keys(fiber.props)
+      .filter(isProperty)
+      .forEach((name) => (dom[name] = fiber.props[name]));
+
+    return dom;
+  }
+
   export function render(
     element: TinyReactElement,
     container: HTMLElement | Text | null
@@ -69,24 +104,82 @@ namespace TinyReact {
       return;
     }
 
-    if (typeof element.type === "string") {
-      const dom: DomType =
-        element.type === "_TEXT_ELEMENT_"
-          ? document.createTextNode("")
-          : document.createElement(element.type);
+    wipRoot = {
+      dom: container,
+      props: {
+        children: [element],
+      },
+      type: undefined,
+    };
 
-      Object.keys(element.props)
-        .filter(isProperty)
-        .forEach((name) => (dom[name] = element.props[name]));
+    nextUnitOfWork = wipRoot;
+  }
 
-      if (Array.isArray(element.props.children)) {
-        element.props.children.forEach((child) => render(child, dom));
-      } else if (element.props.children) {
-        render(element.props.children, dom);
+  let nextUnitOfWork: Fiber | null = null;
+  let wipRoot: Fiber | null = null;
+
+  function workLoop(deadline: IdleDeadline) {
+    let shouldYield = false;
+    while (nextUnitOfWork && !shouldYield) {
+      nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+      shouldYield = deadline.timeRemaining() < 1;
+    }
+    if (!nextUnitOfWork && wipRoot) {
+      console.log("wipRoot = ", wipRoot);
+      wipRoot = null;
+    }
+    requestIdleCallback(workLoop);
+  }
+
+  requestIdleCallback(workLoop);
+
+  function performUnitOfWork(fiber: Fiber): Fiber | null {
+    if (typeof fiber.type === "string") {
+      fiber.dom = createDom(fiber);
+
+      if (fiber.parent) {
+        fiber.parent.dom?.appendChild(fiber.dom);
       }
 
-      container.appendChild(dom);
+      const elements = fiber.props.children;
+      let index = 0;
+      let prevSlibing: Fiber | null = null;
+
+      while (index < elements.length) {
+        const element = elements[index];
+
+        const newFiber: Fiber = {
+          type: element.type,
+          props: element.props,
+          parent: fiber,
+          dom: null,
+        };
+
+        if (index === 0) {
+          fiber.child = newFiber;
+        } else {
+          prevSlibing!.sibling = newFiber;
+        }
+
+        prevSlibing = newFiber;
+        index++;
+      }
+
+      if (fiber.child) {
+        return fiber.child;
+      }
+      let nextFiber: Fiber | null = fiber;
+      while (nextFiber) {
+        if (nextFiber.sibling) {
+          return nextFiber.sibling;
+        }
+        nextFiber = nextFiber.parent || null;
+      }
+
+      return null;
     }
+
+    return null;
   }
 }
 
